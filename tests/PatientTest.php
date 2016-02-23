85a3e7c727a3256b1fde2336706fbfd0c8f97527
Mary Warrington <marywarrington@gmail.com>
2016-02-23 13:45:10 -0800

    Complete passing specs in PateintTest.php

--- tests/PatientTest.php
+++ tests/PatientTest.php
@@ -22,138 +22,136 @@
             Patient::deleteAll();
             Doctor::deleteAll();
         }
-        //
-        // function test_getId()
-        // {
-        //     //Arrange
-        //     $name = "Home Stuff";
-        //     $id = null;
-        //     $test_category = new Doctor ($name, $id);
-        //     $test_category->save();
-        //
-        //     $description = "Wash the dog";
-        //     $category_id = $test_category->getId();
-        //     $test_task = new Patient($description, $id, $category_id);
-        //     $test_task->save();
-        //
-        //     //Act
-        //     $result = $test_task->getId();
-        //
-        //     //Assert
-        //     $this->assertEquals(true, is_numeric($result));
-        // }
-        //
-        // function test_getDoctorId()
-        // {
-        //     //Arrange
-        //     $name = "Home Stuff";
-        //     $id = null;
-        //     $test_category = new Doctor($name, $id);
-        //     $test_category->save();
-        //
-        //     $description = "Wash the dog";
-        //     $category_id = $test_category->getId();
-        //     $test_task = new Patient($description, $id, $category_id);
-        //     $test_task->save();
-        //
-        //     //Act
-        //     $result = $test_task->getDoctorId();
-        //     //Assert
-        //     $this->assertEquals(true, is_numeric($result));
-        // }
-        //
-        // function test_save()
-        // {
-        //     //Arrange
-        //     $name = "Home stuff";
-        //     $id = null;
-        //     $test_category = new Doctor($name, $id);
-        //     $test_category->save();
-        //
-        //     $description = "Wash the dog";
-        //     $category_id = $test_category->getId();
-        //     $test_task = new Patient($description, $id, $category_id);
-        //
-        //     //Act
-        //     $test_task->save();
-        //
-        //     //Assert
-        //     $result = Patient::getAll();
-        //     $this->assertEquals($test_task, $result[0]);
-        // }
-        //
-        // function test_getAll()
-        // {
-        //     //Arrange
-        //     $name = "Home stuff";
-        //     $id = null;
-        //     $test_category = new Doctor($name, $id);
-        //     $test_category->save();
-        //
-        //     $description = "Wash the dog";
-        //     $category_id = $test_category->getId();
-        //     $test_task = new Patient($description, $id, $category_id);
-        //     $test_task->save();
-        //
-        //     $description2 = "Water the lawn";
-        //     $test_task2 = new Patient($description2, $id, $category_id);
-        //     $test_task2->save();
-        //     //Act
-        //     $result = Patient::getAll();
-        //
-        //     //Assert
-        //     $this->assertEquals([$test_task, $test_task2], $result);
-        // }
-        //
-        // function test_deleteAll()
-        // {
-        //     //Arrange
-        //     $name = "Home stuff";
-        //     $id = null;
-        //     $test_category = new Doctor($name, $id);
-        //     $test_category->save();
-        //
-        //     $description = "Wash the dog";
-        //     $category_id = $test_category->getId();
-        //     $test_task = new Patient($description, $id, $category_id);
-        //     $test_task->save();
-        //
-        //     $description2 = "Water the lawn";
-        //     $test_task2 = new Patient($description2, $id, $category_id);
-        //     $test_task2->save();
-        //
-        //     //Act
-        //     Patient::deleteAll();
-        //
-        //     //Assert
-        //     $result = Patient::getAll();
-        //     $this->assertEquals([], $result);
-        // }
-        //
-        //
-        // function test_find()
-        // {
-        //     //Arrange
-        //     $name = "Home stuff";
-        //     $id = null;
-        //     $test_category = new Doctor($name, $id);
-        //     $test_category->save();
-        //
-        //     $description = "Wash the dog";
-        //     $category_id = $test_category->getId();
-        //     $test_task = new Patient($description, $id, $category_id);
-        //     $test_task->save();
-        //
-        //     $description2 = "Water the lawn";
-        //     $test_task2 = new Patient($description, $id, $category_id);
-        //     $test_task2->save();
-        //
-        //     //Act
-        //     $result = Patient::find($test_task->getId());
-        //
-        //     //Assert
-        //     $this->assertEquals($test_task, $result);
-        // }
 
+        function test_getId()
+        {
+            //Arrange
+            $name = "Dr. Joe";
+            $id = null;
+            $test_doctor = new Doctor($name, $id);
+            $test_doctor->save();
+
+            $name1 = "Billy";
+            $doctor_id = $test_doctor->getId();
+            $test_patient = new Patient($name1, $id, $doctor_id);
+            $test_patient->save();
+
+            //Act
+            $result = $test_patient->getId();
+
+            //Assert
+            $this->assertEquals(true, is_numeric($result));
+        }
+
+        function test_getDoctorId()
+        {
+            //Arrange
+            $name = "Dr. Joe";
+            $id = null;
+            $test_doctor = new Doctor($name, $id);
+            $test_doctor->save();
+
+            $name1 = "Billy";
+            $doctor_id = $test_doctor->getId();
+            $test_patient = new Patient($name1, $id, $doctor_id);
+            $test_patient->save();
+
+            //Act
+            $result = $test_patient->getDoctorId();
+            //Assert
+            $this->assertEquals(true, is_numeric($result));
+        }
+
+        function test_save()
+        {
+            //Arrange
+            $name = "Dr. Joe";
+            $id = null;
+            $test_doctor = new Doctor($name, $id);
+            $test_doctor->save();
+
+            $name1 = "Billy";
+            $doctor_id = $test_doctor->getId();
+            $test_patient = new Patient($name1, $id, $doctor_id);
+
+            //Act
+            $test_patient->save();
+
+            //Assert
+            $result = Patient::getAll();
+            $this->assertEquals($test_patient, $result[0]);
+        }
+
+        function test_getAll()
+        {
+            //Arrange
+            $name = "Dr. Joe";
+            $id = null;
+            $test_doctor = new Doctor($name, $id);
+            $test_doctor->save();
+
+            $name1 = "Billy";
+            $doctor_id = $test_doctor->getId();
+            $test_patient = new Patient($name1, $id, $doctor_id);
+            $test_patient->save();
+
+            $name2 = "Bob";
+            $test_patient2 = new Patient($name2, $id, $doctor_id);
+            $test_patient2->save();
+            //Act
+            $result = Patient::getAll();
+
+            //Assert
+            $this->assertEquals([$test_patient, $test_patient2], $result);
+        }
+
+        function test_deleteAll()
+        {
+            //Arrange
+            $name = "Dr. Joe";
+            $id = null;
+            $test_doctor = new Doctor($name, $id);
+            $test_doctor->save();
+
+            $name1 = "Billy";
+            $doctor_id = $test_doctor->getId();
+            $test_patient = new Patient($name1, $id, $doctor_id);
+            $test_patient->save();
+
+            $name2 = "Bob";
+            $test_patient2 = new Patient($name2, $id, $doctor_id);
+            $test_patient2->save();
+
+            //Act
+            Patient::deleteAll();
+
+            //Assert
+            $result = Patient::getAll();
+            $this->assertEquals([], $result);
+        }
+
+        function test_find()
+        {
+            //Arrange
+            $name = "Dr. Joe";
+            $id = null;
+            $test_doctor = new Doctor($name, $id);
+            $test_doctor->save();
+
+            $name1 = "Billy";
+            $doctor_id = $test_doctor->getId();
+            $test_patient = new Patient($name1, $id, $doctor_id);
+            $test_patient->save();
+
+            $name2 = "Bob";
+            $test_patient2 = new Patient($name1, $id, $doctor_id);
+            $test_patient2->save();
+
+            //Act
+            $result = Patient::findById($test_patient->getId());
+
+            //Assert
+            $this->assertEquals($test_patient, $result);
+        }
     }
 ?>
